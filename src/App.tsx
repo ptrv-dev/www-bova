cc13e31b9787f9e06a1040d0ba29d740e1246dd8
ptrv-dev <petrov99119@gmail.com>
2023-01-10 12:57:50 +0200

    Header PC version

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Route, Routes } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
+
 import HomePage from './pages/HomePage';
+
+import Header from './components/Header';
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route index element={<HomePage />} />
       </Routes>
