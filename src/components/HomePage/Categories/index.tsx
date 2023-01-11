cb180d7e6874b38919f2210c3900cbbd26ab27ec
ptrv-dev <petrov99119@gmail.com>
2023-01-11 18:40:53 +0200

    categories update

--- src/components/HomePage/Categories/index.tsx
+++ src/components/HomePage/Categories/index.tsx
@@ -5,15 +5,23 @@ import SectionTitle from '../../UI/SectionTitle';
 import './Categories.scss';
 
 export const categories = [
-  { title: 'Розкидачі мінеральних добрив', categoryId: 1, image: '1.png' },
-  { title: 'Розкидачі органічних добрив', categoryId: 2, image: '1.png' },
-  { title: 'Причепи', categoryId: 3, image: '1.png' },
-  { title: 'Напівпричепи', categoryId: 4, image: '1.png' },
-  { title: 'Фронтальні навантажувачі', categoryId: 5, image: '1.png' },
-  { title: 'Сівалки', categoryId: 6, image: '1.png' },
-  { title: 'Зернометателі', categoryId: 7, image: '1.png' },
-  { title: 'Катки', categoryId: 8, image: '1.png' },
-  { title: 'Борони', categoryId: 9, image: '1.png' },
+  {
+    title: 'Розкидачі мінеральних добрив',
+    categoryId: 1,
+    image: 'мін добрива.png',
+  },
+  {
+    title: 'Розкидачі органічних добрив',
+    categoryId: 2,
+    image: 'орг добрива.png',
+  },
+  { title: 'Причепи', categoryId: 3, image: 'причепи.png' },
+  { title: 'Напівпричепи', categoryId: 4, image: 'напів причепи.png' },
+  { title: 'Фронтальні навантажувачі', categoryId: 5, image: 'куни.png' },
+  { title: 'Сівалки', categoryId: 6, image: 'сівалки.png' },
+  { title: 'Зернометателі', categoryId: 7, image: 'зернометателі.png' },
+  { title: 'Катки', categoryId: 8, image: 'катки.png' },
+  { title: 'Борони', categoryId: 9, image: 'борони.png' },
 ];
 
 const Categories: React.FC = () => {
